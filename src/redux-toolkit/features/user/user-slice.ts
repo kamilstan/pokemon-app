b3ad406926e5f9b395bcde078d436c4c4c2b0875
Kamil Stangret <kamilstangret@wp.pl>
2022-11-07 13:27:48 +0100

    Add redux-toolkit feature: user-slice

---
+++ src/redux-toolkit/features/user/user-slice.ts
@@ -0,0 +1,70 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+interface User {
+    id: string;
+    accessToken: string;
+    expirationTime: number;
+    role: string;
+    isLoggedIn: boolean;
+
+}
+
+const initialState: User = {
+    id: '',
+    accessToken: '',
+    expirationTime: 0,
+    role: '',
+    isLoggedIn: false,
+
+};
+
+interface SetId {
+    payload: string;
+}
+
+interface SetAccessToken {
+    payload: string;
+}
+
+interface SetExpirationTime {
+    payload: number;
+}
+
+interface SetRole {
+    payload: string;
+}
+
+interface SetIsLoggedIn {
+    payload: boolean;
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setId: (state, action: SetId) => {
+            state.id = action.payload;
+        },
+        setAccessToken: (state, action: SetAccessToken) => {
+            state.accessToken = action.payload;
+        },
+        setExpirationTime: (state, action: SetExpirationTime) => {
+            state.expirationTime = action.payload;
+        },
+        setRole: (state, action: SetRole) => {
+            state.role = action.payload;
+        },
+        setIsLoggedIn: (state, action: SetIsLoggedIn) => {
+            state.isLoggedIn = action.payload;
+        },
+    },
+});
+
+export const {
+    setId,
+    setAccessToken,
+    setExpirationTime,
+    setRole,
+    setIsLoggedIn,
+
+} = userSlice.actions;
